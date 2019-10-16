5b5d7136df6df674eebcced780029a1b1bbac70d
Daniel Pokorný <mr.pokorny.d@gmail.com>
2019-10-16 14:58:21 +0200

    vytvoren server

--- src/main/java/mas/mongoface/Mongoface.java
+++ src/main/java/mas/mongoface/Mongoface.java
@@ -1,7 +1,9 @@
 package mas.mongoface;
 
+import org.eclipse.jetty.server.Server;
+
 public class Mongoface {
     public static void main(String[] args) {
-
+        Server server = new Server(8000);
     }
 }
