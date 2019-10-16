43a028b478139c0d5fc4265cc180c639151ada3c
Daniel Pokorný <mr.pokorny.d@gmail.com>
2019-10-16 19:14:30 +0200

    Pridan wsHandler

--- src/main/java/mas/mongoface/Mongoface.java
+++ src/main/java/mas/mongoface/Mongoface.java
@@ -1,9 +1,11 @@
 package mas.mongoface;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 
 public class Mongoface {
     public static void main(String[] args) {
         Server server = new Server(8000);
+        ServletContextHandler wsHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
     }
 }
